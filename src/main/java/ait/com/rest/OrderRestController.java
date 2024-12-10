b192cb6fcffd942f60d1bf47b1d994a21f0ead40
AyushM0207 <ayushmishra0207@gmail.com>
2024-12-10 22:53:03 +0530

    Commited - Ayush Mishra

--- src/main/java/ait/com/rest/OrderRestController.java
+++ src/main/java/ait/com/rest/OrderRestController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import ait.com.entity.Order;
 import ait.com.service.OrderService;
 
-@RestController("/rest")
+@RestController
 public class OrderRestController {
 	@Autowired
 	private OrderService orderService;
